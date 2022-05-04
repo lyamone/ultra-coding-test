cb3b7132c87dd67527c669cdda30862c7cf6ab4d
Lucas <lucas.yamone@gmail.com>
2022-05-04 11:50:43 -0300

    Delete Angular default code
    Change title of the page

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,18 +16,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ultra-coding-test'`, () => {
+  it(`should have as title 'Ultra Coding Test - Lucas Yamone'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ultra-coding-test');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'ultra-coding-test app is running!'
-    );
+    expect(app.title).toEqual('Ultra Coding Test - Lucas Yamone');
   });
 });
