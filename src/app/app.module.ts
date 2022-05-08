05c25bb4727af605746eb229ebab2dc7df410a1b
Lucas <lucas.yamone@gmail.com>
2022-05-08 16:38:33 -0300

    Integrate components in App module
    Integrate components in App module
    Add routing
    Add minor types to HomeComponent and GiphyImagesComponent
    Disable unnecesary type checks in tsconfig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { GiphyApiService } from './giphy-api.service';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { GiphyImagesComponent } from './giphy-images/giphy-images.component';
+import { ToastModule } from './shared/toast/toast.module';
+import { NavbarComponent } from './core/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    GiphyImagesComponent,
+    NavbarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgbModule,
+    ToastModule,
+  ],
+  providers: [GiphyApiService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
